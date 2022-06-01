e302f3dc7da027c0f09b2b886543d1d563ee09ab
charlotte-divine <divinemaina@gmail.com>
2022-06-01 18:33:22 +0100

    add required changes on edit function and clear the input as well

--- src/functinalities.js
+++ src/functinalities.js
@@ -25,6 +25,7 @@ const addmylist = (event) => {
       completed: false,
       index: mylist.length + 1,
     };
+    newTask.value ='';
     mylist = [...mylist, todoElement];
     localStorage.setItem('mylist', JSON.stringify(mylist));
     getmylist();
@@ -34,7 +35,7 @@ const addmylist = (event) => {
 const editmylist = ({ index, event }) => {
   if (event.target.value === '') return;
   if (event.key === 'Enter') {
-    mylist[index].description = event.target.value;
+    mylist[index -1].description = event.target.value;
     localStorage.setItem('mylist', JSON.stringify(mylist));
   }
 };
