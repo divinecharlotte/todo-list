ae03e1f8a870739b5a0cf2cb8bf2915ca6dcbb67
charlotte-divine <divinemaina@gmail.com>
2022-06-06 10:01:45 +0100

    rename folder

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
   })],
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'docs'),
   },
   module: {
     rules: [{
